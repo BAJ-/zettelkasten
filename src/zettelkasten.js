bf44a19057847231a76859f8e18d4baf7758c400
Bjørn Johansen <bjjohans@microsoft.com>
2020-08-31 12:55:17 +0200

    Basic Electron setup

---
+++ src/zettelkasten.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  // Create a browser window
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+// Called when Electron has finished initialization
+// and is ready to create browser windows
+app.whenReady().then(createWindow)
+
+// Close app when all windows are closed except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
