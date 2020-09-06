521a3eefb314d2c8b32aebc2e8e709e79cd1b538
BAJ- <baj-@outlook.com>
2020-09-06 14:42:26 +0200

    Use HashRouter since we are on Electron

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Container } from './components/container';
 import './styles.css';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Container />
-  </BrowserRouter>
+  </HashRouter>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'));
